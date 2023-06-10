ce4bd57b34fb56c0993c17b49455722c3b08d676
chauhanbhargav1273 <bhargavc530@gmail.com>
2023-06-10 10:05:24 +0530

    10-6-23

---
+++ home work/p10.py
@@ -0,0 +1,14 @@
+
+for i in range(9):
+    for j in range(9-i-1):
+        print(" ",end=" ")
+    for j in range(i,-1,-1):
+        print(j+1,end=" ")
+    print(" ")
+
+
+
+
+
+
+
---
+++ home work/p9.py
@@ -0,0 +1,8 @@
+
+for i in range(10):
+    for m in range(i):
+        print(m+1,end=" ")
+    print(" ")
+
+
+
