77b290b1316c8f4e6cb759e4dffa3f5f495fcad7
chauhanbhargav1273 <bhargavc530@gmail.com>
2023-06-10 15:01:31 +0530

    10-6-23

--- home work/p6.py
+++ home work/p6.py
@@ -1,14 +1,6 @@
-
 for i in range(9):
-    for j in range(9-i-1):
+    for j in range(9-i):
         print(" ",end=" ")
     for j in range(i,-1,-1):
         print(i+1,end=" ")
     print("")
-
-
-
-
-
-
-
--- home work/p8.py
+++ home work/p8.py
@@ -1,5 +1,5 @@
 for i in range(9-1,-1,-1):
-    for j in range(9-i-1):
+    for j in range(9-i):
         print(end=" ")
     for j in range(i,-1,-1):
         print(i+1,end=" ")
