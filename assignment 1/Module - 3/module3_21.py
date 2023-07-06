379ecb07af74be38470338cf44fbccfb3a996581
chauhanbhargav1273 <bhargavc530@gmail.com>
2023-07-06 21:34:27 +0530

    6-7-2023

--- assignment 1/Module - 3/module3_21.py
+++ assignment 1/Module - 3/module3_21.py
@@ -1,3 +1,4 @@
+#Write a Python program to convert a tuple to a string.
 t = ('a', 'b', 'c', 'd', 'e')
 a = ''.join(t)
 print(a)
--- assignment 1/Module - 3/module3_43.py
+++ assignment 1/Module - 3/module3_43.py
@@ -2,3 +2,7 @@
 #selecting each letter from a different key in a dictionary.
 
 
+import itertools      
+d ={'1':['a','b'], '2':['c','d']}
+for i in itertools.product(*[d[j] for j in sorted(d.keys())]):
+    print(''.join(i))
